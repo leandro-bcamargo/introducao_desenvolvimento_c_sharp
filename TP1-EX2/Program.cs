eb951502ce24b9264cf19dacb67515f8bbdae99d
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-08 13:58:02 -0300

    TP1-EX2

---
+++ TP1-EX2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Informe o dia do seu nascimento:");
+int dia = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o mês do seu nascimento:");
+int mes = int.Parse(Console.ReadLine());
+
+DateTime hoje = DateTime.Today;
+DateTime proximoAniversario = new DateTime(hoje.Year, mes, dia);
+
+if (proximoAniversario < hoje)
+{
+    proximoAniversario = proximoAniversario.AddYears(1);
+}
+
+TimeSpan diferenca = proximoAniversario - hoje;
+
+int diasRestantes = diferenca.Days;
+
+Console.WriteLine($"Faltam {diasRestantes} dias para o seu aniversário.");
