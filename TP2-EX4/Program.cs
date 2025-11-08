5fc138f06d674ed26c80a4af1956667777361c1f
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-08 16:15:49 -0300

    TP2-EX4

---
+++ TP2-EX4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Crie um programa que solicite nome, idade, telefone e e-mail do usuário e exiba os dados cadastrados de forma organizada. 
+
+//Observações:
+
+//Utilize Console.ReadLine() para entrada de dados.
+//Exiba os dados de forma formatada e clara.
+//Considerações:
+
+//Uso correto de Console.ReadLine().
+//Exibição organizada e formatada das informações.
+
+Console.WriteLine("Digite seu nome:");
+string nome = Console.ReadLine();
+Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite seu telefone:");
+string telefone = Console.ReadLine();
+Console.WriteLine("Digite seu e-mail:");
+string email = Console.ReadLine();
+Console.WriteLine($"Nome: {nome}\n" +
+    $"Idade: {idade}\n" +
+    $"Telefone: {telefone}\n" +
+    $"Email: {email}\n");
