6de8c57a947abbb8eca25ee67e0be6fd64710773
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-08 18:09:01 -0300

    TP2-EX7

---
+++ TP2-EX7/Program.cs
@@ -0,0 +1,20 @@
+﻿//Crie um programa que solicite um número inteiro e determine se ele é par ou ímpar.
+
+//Observações:
+
+//Utilize estruturas condicionais para verificar a paridade.
+//Exiba uma mensagem indicando o resultado.
+//Considerações:
+
+//Uso correto de estruturas condicionais.
+//Cálculo correto da paridade.
+
+Console.WriteLine("Digite um número inteiro:");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"O número {num} é par.");
+} else
+{
+    Console.WriteLine($"O número {num} é ímpar.");
+}
