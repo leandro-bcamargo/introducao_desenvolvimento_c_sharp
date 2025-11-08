ebc93e7c63e15dc484e3a176f46c36c4cb152766
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-08 20:23:19 -0300

    TP2-EX11

---
+++ TP2-EX11/Program.cs
@@ -0,0 +1,16 @@
+﻿//Desenvolva um programa que solicite um número ao usuário e exiba sua tabuada de 1 a 10. 
+
+//Observações:
+
+//Utilize um laço de repetição para calcular a tabuada.
+//Considerações:
+
+//Uso adequado do laço de repetição.
+//Cálculo correto da tabuada.
+
+Console.WriteLine("Digite um número para mostrar sua tabuada:");
+int num = int.Parse(Console.ReadLine());
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{num} * {i} = {i * num}");
+}
