1a3434a587f12a0755803fa8ecb2519c49e44fa8
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-08 20:01:26 -0300

    TP2-EX10

---
+++ TP2-EX10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Crie um programa que solicite um número ao usuário e exiba uma contagem regressiva até 0. 
+
+//Observações:
+
+//Utilize um laço de repetição para realizar a contagem.
+//Exiba os números separados por vírgula.
+//Considerações:
+
+//Uso correto do laço de repetição.
+//Exibição correta da contagem.
+
+Console.WriteLine("Digite um número para a contagem:");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = num; i >= 0; i--)
+{
+    if (i == 0) Console.WriteLine(i);
+    else Console.Write($"{i},");
+}
