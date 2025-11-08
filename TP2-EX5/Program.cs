6d7c10f48ecd0f77a5acfeb2852f671b9375d292
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-08 16:45:28 -0300

    tp2-ex5

---
+++ TP2-EX5/Program.cs
@@ -0,0 +1,17 @@
+﻿// SDesenvolva um programa que peça ao usuário um valor em graus Celsius e o converta para Fahrenheit e Kelvin. 
+
+//Observações:
+
+//Utilize a fórmula: F = C * 9 / 5 + 32 e K = C + 273.15.
+//Exiba os resultados com duas casas decimais.
+//Considerações:
+
+//Cálculo correto das conversões.
+//Exibição formatada dos valores convertidos.
+
+Console.WriteLine("Digite a temperatura em Celsius:");
+double tempCelsius = double.Parse(Console.ReadLine());
+double tempFahrenheit = (tempCelsius * 9 / 5) + 32;
+double tempKelvin = tempCelsius + 273.15;
+
+Console.WriteLine($"A temperatura {tempCelsius}°C corresponde a {tempFahrenheit}°F e {tempKelvin}K");
