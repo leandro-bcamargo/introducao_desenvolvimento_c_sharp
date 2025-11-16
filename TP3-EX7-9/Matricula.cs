e40e977e335901e3614b344bcc30f7f31b3c8843
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-16 20:33:44 -0300

    TP3-EX7

---
+++ TP3-EX7-9/Matricula.cs
@@ -0,0 +1,14 @@
+﻿class Matricula
+{
+    private string _nomeDoAluno;
+    private string _curso;
+    private int _numeroMatricula;
+    private string _situacao;
+    private string _dataInicial;
+
+    public string NomeDoAluno { get { return _nomeDoAluno;} set { _nomeDoAluno = value;} }
+    public string Curso { get { return _curso; } set { _curso = value; } }
+    public int NumeroMatricula { get { return _numeroMatricula; } set { _numeroMatricula = value; } }
+    public string Situacao { get { return _situacao;} set { _situacao = value; } }
+    public string DataInicial { get { return _dataInicial; } set { _dataInicial = value; } }
+}
