e4ea7694f9dba76d50513c5dda2e4dc8cb3ccd2b
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-16 20:53:44 -0300

    TP3-EX9

---
+++ TP3-EX7-9/MatriculaApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_EX7_9
+{
+    internal class MatriculaApp
+    {
+        public static void Main()
+        {
+            Matricula matricula = new Matricula();
+            matricula.NomeDoAluno = "Leandro Camargo";
+            matricula.Curso = "ADS";
+            matricula.NumeroMatricula = 12345;
+            matricula.Situacao = "Pendente";
+            matricula.DataInicial = "16/11/2025";
+
+            matricula.Trancar();
+            Console.WriteLine(matricula.Situacao);
+            matricula.Reativar();
+            Console.WriteLine(matricula.Situacao);
+
+            matricula.ExibirInformacoes();
+
+        }
+    }
+}
