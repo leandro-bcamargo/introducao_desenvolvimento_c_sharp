0957f2c5ad875a59f906c2c36eaca89ee9868520
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-16 20:47:10 -0300

    TP3-EX8

--- TP3-EX7-9/Matricula.cs
+++ TP3-EX7-9/Matricula.cs
@@ -11,4 +11,24 @@
     public int NumeroMatricula { get { return _numeroMatricula; } set { _numeroMatricula = value; } }
     public string Situacao { get { return _situacao;} set { _situacao = value; } }
     public string DataInicial { get { return _dataInicial; } set { _dataInicial = value; } }
+
+    public void Trancar()
+    {
+        Situacao = "Trancada";
+    }
+
+    public void Reativar()
+    {
+        Situacao = "Ativa";
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome do aluno: {NomeDoAluno}\n" +
+            $"Curso: {Curso}\n" +
+            $"Situação atual: {Situacao}\n" +
+            $"Data inicial: {DataInicial}");
+    }
+
+
 }
