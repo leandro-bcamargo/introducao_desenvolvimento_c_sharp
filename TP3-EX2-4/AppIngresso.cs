5e36c3d9d1a8354e467245c46d758a0a784a7725
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-16 13:57:47 -0300

    TP3-EX4

---
+++ TP3-EX2-4/AppIngresso.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_EX2
+{
+    internal class AppIngresso
+    {
+        public static void Main()
+        {
+            Ingresso ingresso = new Ingresso();
+            ingresso.Preco = 100;
+            ingresso.NomeDoShow = "Ozzy";
+            ingresso.QuantidadeDisponivel = 5000;
+
+            ingresso.AlterarPreco(150);
+            ingresso.AlterarQuantidade(10000);
+
+            ingresso.ExibirInformacoes();
+        }
+    }
+}
---
+++ TP3-EX2-4/Ingresso.cs
@@ -0,0 +1,27 @@
+﻿class Ingresso
+{
+    private string _nomeDoShow; // identifica o show a que o ingresso se refere
+    private double _preco; // identifica o preço do ingresso
+    private int _quantidadeDisponivel; // identifica quantos ingressos estão disponíveis. 
+
+    public string NomeDoShow { get { return _nomeDoShow; } set { _nomeDoShow = value; } }
+    public double Preco { get { return _preco; } set { _preco = value; } }
+    public int QuantidadeDisponivel { get { return _quantidadeDisponivel;} set { _quantidadeDisponivel = value; } }
+
+    public void AlterarPreco(double preco)
+    {
+        Preco = preco;
+    }
+
+    public void AlterarQuantidade(int novaQuantidade)
+    {
+        QuantidadeDisponivel = novaQuantidade;
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome do show: {NomeDoShow}\n" +
+            $"Preço: {Preco}\n" +
+            $"Quantidade disponível: {QuantidadeDisponivel}");
+    }
+}
