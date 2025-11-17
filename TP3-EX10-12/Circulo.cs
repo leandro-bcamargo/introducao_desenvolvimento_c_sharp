0e74ab195c650b5aa0995de42d00a6f88e7dab08
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-11-16 21:01:37 -0300

    TP3-EX10

---
+++ TP3-EX10-12/Circulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_EX10_12
+{
+    internal class Circulo
+    {
+        // Um círculo precisa de um raio e de um ponto de origem para ser definido, logo o raio é um atributo essencial.
+        private double _raio;
+
+        public double Raio { get { return _raio; } set { _raio = value; } }
+    }
+}
---
+++ TP3-EX10-12/Esfera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_EX10_12
+{
+    internal class Esfera
+    {
+        // Uma esfera precisa de um raio e de um ponto de origem para ser definida, logo o raio é um atributo essencial.
+        private double _raio;
+
+        public double Raio
+        {
+        get { return _raio; }
+        set { _raio = value; } }
+        }
+
+}
